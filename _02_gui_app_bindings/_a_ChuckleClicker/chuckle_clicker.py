de871350b0167af625a1d5a1e356f1f63b89b6eb
jeremycui <jeremycuid@gmail.com>
2024-02-28 19:57:18 -0800

    cool

--- _02_gui_app_bindings/_a_ChuckleClicker/chuckle_clicker.py
+++ _02_gui_app_bindings/_a_ChuckleClicker/chuckle_clicker.py
@@ -4,6 +4,7 @@
 import tkinter as tk
 import random
 from tkinter import messagebox
+import pymouse
 
 
 # Use this function to return a random character
@@ -24,6 +25,9 @@ class ChuckleClicker(tk.Tk):
         #  method when a mouse button is pressed
         #  example: self.joke_button.bind('<ButtonPress>', self.on_joke)
         self.on_joke.bind()
+        button = pymouse()
+
+
         # TODO: Call the joke button's bind() method to call the on_punchline()
         #  method when a mouse button is pressed
 
